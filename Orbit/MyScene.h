bc835fdf47f3eab6184a745ef40cd0a5899efdb9
Christian Ayscue <christianayscue@Christians-MacBook-Pro.local>
2014-08-08 12:05:43 -0700

    Initial Commit

---
+++ Orbit/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Orbit
+//
+
+//  Copyright (c) 2014 coayscue. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ Orbit/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Orbit
+//
+
+//  Copyright (c) 2014 coayscue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
